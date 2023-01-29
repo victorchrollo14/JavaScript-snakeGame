c19c60a106798595c88dcdcf74541ebe4b545869
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-01-29 17:06:54 +0530

    done with adding top bottom left and right wall

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let food = new Audio('music/food.mp3');
+let foodSound = new Audio('music/food.mp3');
 let gameover = new Audio('music/gameover.mp3');
 let move = new Audio('music/move.mp3');
 let music = new Audio('music/music.mp3');
 
-
+let board = document.getElementById('board');
+let xMin = board.getBoundingClientRect().top;
+let xMax = board.getBoundingClientRect().bottom;
+console.log(xMin, xMax);
